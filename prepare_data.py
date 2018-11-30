6c914f51081d7fe27f1b79040b594eacb5128c7e
Garrett Smith <g@rre.tt>
2018-11-30 16:05:53 -0600

    Stub for Guild

---
+++ prepare_data.py
@@ -0,0 +1,8 @@
+import preprocess
+
+def main():
+    feature_dim_2 = 11
+    preprocess.save_data_to_array(max_len=feature_dim_2)
+
+if __name__ == "__main__":
+    main()
---
+++ train.py
@@ -0,0 +1,10 @@
+import preprocess
+
+def main():
+    (X_train,
+     X_test,
+     y_train,
+     y_test) = preprocess.get_train_test()
+
+if __name__ == "__main__":
+    main()
